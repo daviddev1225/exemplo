c87d77d3ee9a1e3839636ac06248696eecfae905
David Anderson <david.softwares1@gmail.com>
2024-09-12 01:21:03 -0300

    primeiros passos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from "react";
+import {createStore} from "redux";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reducer = function(state,action){
+  if(action.type === "INC"){
+    return state + action.payload;
+  }else if(action.type === "DESC"){
+    return state - action.payload;
+  }
+  return state;
+}
+const store = createStore(reducer,0);
+store.subscribe(function(){
+  console.log("O estado mudou!", store.getState());
+})
+
+store.dispatch({type:'INC',payload:2});
+store.dispatch({type:'INC',payload:2});
+store.dispatch({type:'INC',payload:2});
+store.dispatch({type:'DESC',payload:2});
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {count:0}
+    this.increment = this.increment.bind(this);
+  }
+
+  increment = ()=>{
+    this.setState({count:this.state.count+1}) 
+  }
+
+  decrement = ()=>{
+    this.setState({count:this.state.count-1})
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.increment}>Mais</button>
+        <button onClick={this.decrement}>Menos</button>
+      </div>
+    )
+  }
 }
 
 export default App;
